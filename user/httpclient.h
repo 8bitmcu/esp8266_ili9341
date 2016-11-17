0907ad4f1bca50c2e4913ad16fcc81398bc5afa9
Unknown <agashka2@gmail.com>
2016-11-17 19:41:34 -0400

    lwip & custom httpclient
    barely functioning prototype

--- user/httpclient.h
+++ user/httpclient.h
@@ -1,52 +1,45 @@
-/*
- * ----------------------------------------------------------------------------
- * "THE BEER-WARE LICENSE" (Revision 42):
- * Martin d'Allens <martin.dallens@gmail.com> wrote this file. As long as you retain
- * this notice you can do whatever you want with this stuff. If we meet some day,
- * and you think this stuff is worth it, you can buy me a beer in return.
- * ----------------------------------------------------------------------------
- */
-
 #ifndef HTTPCLIENT_H
 #define HTTPCLIENT_H
 
-//#include <espmissingincludes.h> // This can remove some warnings depending on your project setup. It is safe to remove this line.
 
-#define HTTP_STATUS_GENERIC_ERROR  -1   // In case of TCP or DNS error the callback is called with this status.
-#define BUFFER_SIZE_MAX            5000 // Size of http responses that will cause an error.
+#define LWIP_OPEN_SRC
 
-/*
- * "full_response" is a string containing all response headers and the response body.
- * "response_body and "http_status" are extracted from "full_response" for convenience.
- *
- * A successful request corresponds to an HTTP status code of 200 (OK).
- * More info at http://en.wikipedia.org/wiki/List_of_HTTP_status_codes
- */
-typedef void (* http_callback)(char * response_body, int http_status, char * response_headers, int body_size);
+#include "user_interface.h"
 
-/*
- * Download a web page from its URL.
- * Try:
- * http_get("http://wtfismyip.com/text", http_callback_example);
- */
-void ICACHE_FLASH_ATTR http_get(const char * url, const char * headers, http_callback user_callback);
+#include "lwip/opt.h"
+#include "lwip/ip.h"
+#include "lwip/sockets.h"
+#include "lwip/ip_addr.h"
+#include "lwip/init.h"
+#include "lwip/inet.h"
+#include "lwip/netif.h"
+#include "lwip/igmp.h"
+#include "lwip/udp.h"
+#include "lwip/tcp.h"
 
-/*
- * Post data to a web form.
- * The data should be encoded as application/x-www-form-urlencoded.
- * Try:
- * http_post("http://httpbin.org/post", "first_word=hello&second_word=world", http_callback_example);
- */
-void ICACHE_FLASH_ATTR http_post(const char * url, const char * post_data, const char * headers, http_callback user_callback);
 
-/*
- * Call this function to skip URL parsing if the arguments are already in separate variables.
- */
-void ICACHE_FLASH_ATTR http_raw_request(const char * hostname, int port, bool secure, const char * path, const char * post_data, const char * headers, http_callback user_callback);
 
-/*
- * Output on the UART.
- */
-void http_callback_example(char * response_body, int http_status, char * response_headers, int body_size);
+//TODO: refactor
+#define espconn_printf(fmt, args...) os_printf(fmt,## args)
+
+
+
+static err_t ICACHE_FLASH_ATTR
+httpclient_sent(void *arg, struct tcp_pcb *pcb, u16_t len);
+
+
+static err_t ICACHE_FLASH_ATTR
+httpclient_recv(void *arg, struct tcp_pcb *pcb, struct pbuf *p, err_t err);
+
+
+static err_t ICACHE_FLASH_ATTR
+httpclient_connected(void *arg, struct tcp_pcb *tpcb, err_t err);
+
+
+static void ICACHE_FLASH_ATTR
+httpclient_err(void *arg, err_t err);
+
+
+bool httpclient_connect(uint8_t *ip, uint16_t port);
 
 #endif
--- user/user_main.c
+++ user/user_main.c
@@ -3,16 +3,21 @@
 #include "gpio.h"
 #include "os_type.h"
 #include "user_config.h"
-#include "user_interface.h"
-#include "espconn.h"
 #include "uart.h"
 
-#include "spi.h"
-#include "ili9341.h"
+
+#define LWIP_OPEN_SRC
+
+#include "user_interface.h"
 #include "httpclient.h"
+
+
+
 
 #define user_procTaskPrio        0
 #define user_procTaskQueueLen    1
+
+
 os_event_t    user_procTaskQueue[user_procTaskQueueLen];
 static void user_procTask(os_event_t *events);
 
@@ -20,44 +25,16 @@ static volatile os_timer_t some_timer;
 
 
 
-//uint16_t color = ILI9340_GREEN;
-
 void some_timerfunc(void *arg) {
 
-  //color = color == ILI9340_GREEN ? ILI9340_BLUE : ILI9340_GREEN;
+    // connect to client
+    uint8_t ip[4] = {192, 168, 0, 11};
+    httpclient_connect(ip, 80);
 
-
-    //fillScreen(color);
-
-    os_printf("Trying wifi...");
-
-    struct station_config stationConf;
-
-    wifi_set_opmode( STATION_MODE );
-    os_memcpy(&stationConf.ssid, SSID_NAME, 32);
-    os_memcpy(&stationConf.password, SSID_PWD, 32);
-    wifi_station_set_config(&stationConf);
-    wifi_station_connect();
-
-
-    http_get("http://api.ipify.org/?format=text", "", http_callback_example);
 
 
     os_delay_us(100*1000);
 }
-
-
-/*
-void http_callback_example(char * response_body, int http_status, char * response_headers, int body_size)
-{
-    os_printf("http_status=%d\n", http_status);
-    if (http_status != HTTP_STATUS_GENERIC_ERROR) {
-        os_printf("strlen(headers)=%d\n", strlen(response_headers));
-        os_printf("body_size=%d\n", body_size);
-        os_printf("body=%s<EOF>\n", response_body);
-    }
-}
-*/
 
 
 //Do nothing function
@@ -73,7 +50,22 @@ void ICACHE_FLASH_ATTR user_init() {
 
     uart_init(BIT_RATE_9600, BIT_RATE_9600);
 
-    ili_init(true);
+
+
+
+    // init WIFI
+
+ 
+    struct station_config stationConf; 
+ 
+    wifi_set_opmode( 0x01 ); 
+    os_memcpy(&stationConf.ssid, SSID_NAME, 32); 
+    os_memcpy(&stationConf.password, SSID_PWD, 32); 
+    wifi_station_set_config(&stationConf); 
+    wifi_station_connect(); 
+
+
+
 
     //Disarm timer
     os_timer_disarm(&some_timer);
